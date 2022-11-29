2b730d7d6f9ea72f81212c6817e24404276005af
unsignd <unsigndid@gmail.com>
2022-11-29 19:38:41 +0900

    remove uuid

--- src/main/java/me/yejoon/yubusmp/EventListener.java
+++ src/main/java/me/yejoon/yubusmp/EventListener.java
@@ -9,16 +9,16 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class EventListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        e.setJoinMessage(e.getPlayer().getName() + " (" + e.getPlayer().getUniqueId() + ") < 이사람 서버접속함");
+        e.setJoinMessage(e.getPlayer().getName() + " < 이사람 서버접속함");
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
-        e.setQuitMessage(e.getPlayer().getName() + " (" + e.getPlayer().getUniqueId() + ") < 이사람 서버나감");
+        e.setQuitMessage(e.getPlayer().getName() + " < 이사람 서버나감");
     }
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
-            e.setDeathMessage(e.getPlayer().getName() + " (" + e.getPlayer().getUniqueId() + ") < 이사람 죽어버림");
+            e.setDeathMessage(e.getPlayer().getName() + " < 이사람 죽어버림");
     }
 }
