5249c1e443a7615cbbe816afcae46ea5002d1311
unsignd <unsigndid@gmail.com>
2022-11-29 20:46:45 +0900

    fix bug22

--- src/main/java/me/yejoon/yubusmp/EventListener.java
+++ src/main/java/me/yejoon/yubusmp/EventListener.java
@@ -2,6 +2,7 @@ package me.yejoon.yubusmp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -24,7 +25,7 @@ public class EventListener implements Listener {
             int randomZ = randomPos.nextInt(100000) - 50000;
             Block block = Bukkit.getWorld("World").getHighestBlockAt(randomPos.nextInt(50000) - 25000 + randomX, randomPos.nextInt(50000) - 25000 + randomZ);
             e.getPlayer().teleport(block.getLocation().add(.5, 1, .5));
-            e.getPlayer().setBedSpawnLocation(block.getLocation().add(.5, 1, .5));
+            e.getPlayer().setBedSpawnLocation(block.getLocation().add(.5, 1, .5), true);
         }
     }
 
@@ -35,6 +36,8 @@ public class EventListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
-            e.setDeathMessage(e.getPlayer().getName() + " < 이사람 죽어버림");
+        if (e.getEntity() instanceof Player) {
+            e.setDeathMessage(e.getEntity().getName() + " < 이사람 죽어버림");
+        }
     }
 }
