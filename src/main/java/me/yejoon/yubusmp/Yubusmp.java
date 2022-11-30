6368c11883ec3363ebbcb033f01503ed328e6f08
unsignd <unsigndid@gmail.com>
2022-11-30 12:34:06 +0900

    edit itemMeta

--- src/main/java/me/yejoon/yubusmp/Yubusmp.java
+++ src/main/java/me/yejoon/yubusmp/Yubusmp.java
@@ -1,8 +1,10 @@
 package me.yejoon.yubusmp;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -13,11 +15,17 @@ public final class Yubusmp extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
 
-        ShapedRecipe compass = new ShapedRecipe(new ItemStack(Material.COMPASS));
+        ItemStack comp = new ItemStack(Material.COMPASS);
+        ItemMeta compMeta = comp.getItemMeta();
+        compMeta.setDisplayName(ChatColor.RED + "플레이어 추적기");
+        comp.setItemMeta(compMeta);
+
+        ShapedRecipe compass = new ShapedRecipe(comp);
         compass.shape("EDE", "DWD", "EDE");
         compass.setIngredient('E', Material.ENDER_EYE);
         compass.setIngredient('D', Material.DIAMOND_BLOCK);
-        compass.setIngredient('W', Material.WITHER_SKELETON_SKULL, 3);
+        compass.setIngredient('W', Material.WITHER_SKELETON_SKULL);
+
         getServer().addRecipe(compass);
 
         getLogger().info("YUBU SMP PLUGIN ENABLED");
