c61f51d87a5a06699007cc803e7cb0e1fcbfe55f
unsignd <unsigndid@gmail.com>
2022-11-30 10:02:39 +0900

    fix bug333

--- src/main/java/me/yejoon/yubusmp/EventListener.java
+++ src/main/java/me/yejoon/yubusmp/EventListener.java
@@ -1,20 +1,28 @@
 package me.yejoon.yubusmp;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
 import static org.bukkit.Bukkit.getLogger;
 
 
 public class EventListener implements Listener {
+    Map<UUID, Location> spawn = new HashMap<UUID, Location>();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.setJoinMessage(e.getPlayer().getName() + " < 이사람 서버접속함");
@@ -25,7 +33,7 @@ public class EventListener implements Listener {
             int randomZ = randomPos.nextInt(10000) - 5000;
             Block block = Bukkit.getWorld("World").getHighestBlockAt(randomPos.nextInt(5000) - 2500 + randomX, randomPos.nextInt(5000) - 2500 + randomZ);
             e.getPlayer().teleport(block.getLocation().add(.5, 1, .5));
-            e.getPlayer().setBedSpawnLocation(block.getLocation().add(.5, 1, .5), true);
+            spawn.put(e.getPlayer().getUniqueId(), block.getLocation().add(.5, 1, .5));
         }
     }
 
@@ -39,5 +47,15 @@ public class EventListener implements Listener {
         if (e.getEntity() instanceof Player) {
             e.setDeathMessage(e.getEntity().getName() + " < 이사람 죽어버림");
         }
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent e) {
+        e.setRespawnLocation(spawn.get(e.getPlayer().getUniqueId()));
+    }
+
+    @EventHandler
+    public void onEnterBed(PlayerBedEnterEvent e) {
+        spawn.put(e.getPlayer().getUniqueId(), e.getBed().getLocation());
     }
 }
