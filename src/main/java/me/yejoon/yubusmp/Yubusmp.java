123a1ba16f942a311af4bc4d5165450ea4b438c2
unsignd <unsigndid@gmail.com>
2022-11-30 12:20:31 +0900

    add recipe

--- src/main/java/me/yejoon/yubusmp/Yubusmp.java
+++ src/main/java/me/yejoon/yubusmp/Yubusmp.java
@@ -1,12 +1,25 @@
 package me.yejoon.yubusmp;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Yubusmp extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new EventListener(), this);
+
+        ShapedRecipe compass = new ShapedRecipe(new ItemStack(Material.COMPASS));
+        compass.shape("EDE", "DWD", "EDE");
+        compass.setIngredient('E', Material.ENDER_EYE);
+        compass.setIngredient('D', Material.DIAMOND_BLOCK);
+        compass.setIngredient('W', Material.WITHER_SKELETON_SKULL, 3);
+        getServer().addRecipe(compass);
+
         getLogger().info("YUBU SMP PLUGIN ENABLED");
     }
 
